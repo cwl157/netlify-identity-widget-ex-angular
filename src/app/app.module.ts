368be2d771b8c571d365c30ecf7f07cb04269f83
Carl Layton <cwl157@gmail.com>
2019-03-21 21:46:42 -0500

    Clean up notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-// XX TODO: Figure out how to make a elements look like links. Empty href doesnt' work
-// XX TODO: Figure out how to reload the current page on login N/A
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
